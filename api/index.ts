e8a7864a2c76c95d4878a9036784bad70208f8bc
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-27 15:31:53 +0700

    fix type

--- api/index.ts
+++ api/index.ts
@@ -7,27 +7,7 @@ const app: Application = express();
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  const persons = new Persons<Person>({
-    name: "Ahmad",
-    birth: "23 july 2005",
-    address: {
-      street: "jln karimun",
-      city: "Cilacap"
-    },
-    educations: [
-      {
-        name: "SMK Darul Ulum",
-        level: "Vacation High school",
-        address: "Sidareja",
-        year: "2020-2023"
-      }
-    ],
-    hobbies: [
-      "coding",
-      "drawing"
-    ]
-  });
-  res.json(persons.person);
+  res.json({status: true});
 });
 
 app.listen(8080, () => {
