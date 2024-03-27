038f792d247bacbc854aac79ce91125cba7232b5
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-27 15:44:52 +0700

    fix type

--- api/index.ts
+++ api/index.ts
@@ -10,7 +10,27 @@ const port: number = process.env.PORT || 8080;
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
-  res.json({status: true});
+  const persons = new Persons<Person>({
+    name: "Ahmad",
+    birth: "23 july 2005",
+    address: {
+      street: "jln karimun",
+      city: "Cilacap"
+    },
+    educations: [
+      {
+        name: "SMK Darul Ulum",
+        level: "Vacation High school",
+        address: "Sidareja",
+        year: "2020-2023"
+      }
+    ],
+    hobbies: [
+      "coding",
+      "drawing"
+    ]
+  });
+  res.json(persons.person);
 });
 
 app.listen(port, '0.0.0.0', () => {
