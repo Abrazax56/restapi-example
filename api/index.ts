a0aa3007a6864c47bd9a33ba8ff27acd279a4d7a
abrazax56 <ahmadbenirusli@gmail.com>
2024-03-27 15:41:27 +0700

    fix type

--- api/index.ts
+++ api/index.ts
@@ -2,14 +2,17 @@ import express, { Application, Request, Response, NextFunction } from "express";
 import cors from 'cors';
 import { Person } from '.././types/person';
 import { Persons } from '.././class/Person';
+import 'dotenv/config';
 
 const app: Application = express();
+const port: number = process.env.PORT || 8080;
+
 app.use(cors());
 
 app.get('/', (req: Request, res: Response) => {
   res.json({status: true});
 });
 
-app.listen(8080, () => {
+app.listen(port, '0.0.0.0', () => {
   console.info('app is listening...');
 })
