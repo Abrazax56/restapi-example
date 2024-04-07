2e05c6fc7f895c03c128370f9af401c118a08566
abrazax56 <ahmadbenirusli@gmail.com>
2024-04-07 15:17:25 +0700

    add auth headers

--- src/Routers/UserRouters.ts
+++ src/Routers/UserRouters.ts
@@ -63,13 +63,12 @@ UserRouter.put('/user/:options', async(req: Request, res: Response) => {
           password: req.body.password
         });
         const token = await loggingin.login();
-        res.cookie("USER_AUTH", token, {
+        res.status(200).cookie("USER_AUTH", token, {
           httpOnly: false,
           secure: true,
           maxAge: 1000*60*60*24*30,
           expires: new Date(Date.now() + 1000*60*60*24*20)
-        });
-        res.status(200).json({
+        }).json({
           status: 200,
           message: "successfully login"
         })
@@ -77,13 +76,12 @@ UserRouter.put('/user/:options', async(req: Request, res: Response) => {
       case 'logout':
         const loggingout = new Logout<string>((req.headers.userauth) as string);
         await loggingout.logout();
-        res.cookie("USER_AUTH", req.cookies.USER_AUTH, {
+        res.status(200).cookie("USER_AUTH", req.cookies.USER_AUTH, {
           httpOnly: false,
           secure: true,
           maxAge: 100,
           expires: new Date(Date.now() + 10)
-        });
-        res.status(200).json({
+        }).json({
           status: 200,
           message: 'logout successfully'
         })
